9278a1b50462e423d6d0920c7eb64888d0841a81
kancla <99520538+kancla@users.noreply.github.com>
2022-03-15 11:22:57 -0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -12,42 +12,52 @@ sleep 2
 USER=root
 
 if
-	[ $USER != root ]
+[ $USER != root ]
 
-	then
-	echo -e "\n please you need to be a root user to run the installation.\n"
-	exit 1
+then
+echo -e "\n please you need to be a root user to run the installation.\n"
+exit 1
 fi
-                                                                                                                                               
+sleep 2
+
+echo -e "\n uninstall old versions \n"
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sleep 2
+
+echo -e "\n let's set up the repository \n"
+																				
  sleep 2
-																		                        echo -e "\n uninstall old versions \n"
-																		                        sudo apt-get remove docker docker-engine docker.io containerd runc
-																				    sleep 2
 
+sudo apt-get update
+
+sleep 2
+
+sudo apt-get install ca-certificates curl gnupg lsb-release																																
+
+sleep 2
+
+echo -e "\n add Docker's official GPG key \n"
 																				
-																				    echo -e "\n let's set up the repository \n"
+sleep 2															
 
-																				
-																				    sleep 2
-																		                     sudo apt-get update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-   sleep 2
+sleep 2																					
 
-																				
-   sudo apt-get install ca-certificates curl gnupg lsb-release																																
-   sleep 2
-																																								
-   echo -e "\n add Docker's official GPG key \n"
-																				
-   sleep 2																																						               curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-																																						                        sleep 2
-																																									echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-																																									sleep 2
-																																									echo -e "\n install docker engine \n"
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null																							
 
-																																						                        sleep 2
-																			
-																																									sudo apt-get update
-																																																										                        sudo apt-get install docker-ce docker-ce-cli containerd.io
-																																																										                        sudo docker run hello-world
-																																						                        sleep 2																																																																														                        echo -e  "\n docker successfully install.thank you \n"
+sleep 2
+
+echo -e "\n install docker engine \n"	
+
+sleep 2
+													
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
+
+sleep 2
+
+echo -e  "\n docker successfully install.thank you \n"
